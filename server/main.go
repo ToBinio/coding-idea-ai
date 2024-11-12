27e1eb1738896cc72687463098a0fc4ab25c10d8
tobinio <Tobias.frischmann1@gmail.com>
2024-11-12 17:21:43 +0100

    start prompting ai

--- server/main.go
+++ server/main.go
@@ -30,11 +30,36 @@ func hello(c *gin.Context) {
 		return
 	}
 
+	res, err := getAiResponse(newChatMessage.Text, newChatMessage.Context)
+	if err != nil {
+		log.Printf("error: %s", err.Error())
+		return
+	}
+
+	log.Printf("response: %s", res["response"])
+
+	c.JSON(200, gin.H{"response": res["response"], "context": res["context"]})
+}
+
+func getAiResponse(text string, context []int) (map[string]interface{}, error) {
 	values := map[string]interface{}{
-		"model":   "llama3.2",
-		"system":  "your job is to give short answers. Keep your answers as short as possible!",
-		"prompt":  newChatMessage.Text,
-		"context": newChatMessage.Context,
+		"model": "llama3.2",
+		"system": `
+Your task is to help users find new and unique coding projects. Use your creativity!
+You have 10 Questions to find a good project idee.
+Start with very broad questions and get more precise as you go. 
+After 10 Questions the 11 Question should be what project idee the user wants to choose.
+
+Provide answers in a specific structure, separated into three sections, with each section clearly separated by a Header.
+The headers should be "**Thoughts**", "**Question**" and "**Answers**"
+Do Not add any sort of Chapter Title do only use "**Thoughts**", "**Question**" and "**Answers**"
+
+    First section: Provide your thoughts.
+    Second section: Pose a question to the user to clarify their interests or objectives.
+    Third section: List the possible answers to your question, each as a single line with a * as a element marker.
+		`,
+		"prompt":  text,
+		"context": context,
 		"stream":  false,
 	}
 	jsonData, err := json.Marshal(values)
@@ -47,8 +72,7 @@ func hello(c *gin.Context) {
 		bytes.NewBuffer(jsonData))
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -56,8 +80,5 @@ func hello(c *gin.Context) {
 	var res map[string]interface{}
 
 	json.NewDecoder(resp.Body).Decode(&res)
-
-	fmt.Println(res["response"])
-
-	c.JSON(200, gin.H{"response": res["response"], "context": res["context"]})
+	return res, nil
 }
